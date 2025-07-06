e47c881039eaa31bf9b81680d9a2efd423aa4566
russellpeters79 <russellpeters79@googlemail.com>
2025-07-06 13:34:04 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('contactButton');
+    button.addEventListener('click', () => {
+        alert('Call +30 697 813 1329');
+    });
+});
